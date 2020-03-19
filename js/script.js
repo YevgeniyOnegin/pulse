1ed4950e29390f3d016509891a9e62b2e83c8445
Yevgeniy Onegin <yevgeniybabanin@gmail.com>
2020-03-19 15:29:27 +0300

    add modals

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+$(document).ready(function() {
     $('.carousel__inner').slick({
         speed: 1200,
         autoplay: true,
@@ -23,7 +23,7 @@ $(document).ready(function(){
 
     function toggleSlide (item) {
         $(item).each(function(i) {
-            $(this).on('click', function(e){
+            $(this).on('click', function(e) {
                   e.preventDefault();
                   $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
                   $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
@@ -33,4 +33,23 @@ $(document).ready(function(){
     toggleSlide('.catalog-item__link');
     toggleSlide('.catalog-item__back');
 
+    //modal
+
+    $('[data-modal=consultation]').on('click', function() {
+        $('.overlay, #consultation').fadeIn('slow');
+    });
+
+    $('.modal__close').on('click', function() {
+        $('.overlay, #consultation, #order, #thanks').fadeOut('slow');
+    });
+
+    // $('.button_mini').on('click', function() {
+    //     $('.overlay, #order').fadeIn('slow');
+    // });
+    $('.button_mini').each(function(i) {
+        $(this).on('click', function() {
+            $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+            $('.overlay, #order').fadeIn('slow');
+        });
+    });
 });
