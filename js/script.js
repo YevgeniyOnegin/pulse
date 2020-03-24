8a785a5351be8ec068056959cb12f3df9293b866
Yevgeniy Onegin <yevgeniybabanin@gmail.com>
2020-03-24 16:33:21 +0300

    final edits_1

--- js/script.js
+++ js/script.js
@@ -98,4 +98,26 @@ $(document).ready(function() {
         });
         return false;
     });
+
+    //scrollup
+
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 1600) {
+            $('.pageup').fadeIn();
+        } else {
+            $('.pageup').fadeOut();
+        }
+    });
+
+    //smooth scroll
+
+    $("a[href=#up]").click(function(){
+        const _href = $(this).attr("href");
+        $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+        return false;
+    });
+
+    //wow.js
+
+    new WOW().init();
 });
